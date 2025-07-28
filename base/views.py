c35bf9e52f077ca0357122bac197d03eb023b217
Garvit Agrawal <garvitagrawal321.delhi@gmail.com>
2025-07-29 00:58:14 +0530

    Initial Django project

---
+++ base/views.py
@@ -0,0 +1,40 @@
+from django.contrib import  messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .models import Room
+# from django.http import HttpResponse
+
+# rooms = [
+#     {'id': 1, 'name': 'Room 1'},
+#     {'id': 2, 'name': 'Room 2'},
+#     {'id': 3, 'name': 'Room 3'},
+# ]
+def loginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user_obj = User.objects.get(email=email)
+        except User.DoesNotExist:
+            messages.error(request, "User does not exist.")
+            return render(request, 'base/login_register.html', {})
+        user = authenticate(request, username=user_obj, password=password)  
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Wrong User ID and Password.")
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def home(request):
+    rooms = Room.objects.all()  
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk) 
+    context = {'rooms': room}
+    return render(request, 'base/room.html', context)     
