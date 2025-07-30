4be99063d022b2c48a9e0a6ed07c0878163459b5
Garvit Agrawal <garvitagrawal321.delhi@gmail.com>
2025-07-30 20:01:00 +0530

    Changes

--- base/urls.py
+++ base/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
-path('student_reg/', views.student_reg, name="student_reg"),
+    path('student_reg/', views.student_reg, name="student_reg"),
     path('', views.home, name="home"),
-    # path('room/<str:pk>/', views.room, name="room"),
     path('login/studentdash/', views.studentdashboard, name="studentdash"),
     path('login/admindash/', views.admindashboard, name="admindash"),
-    # path('room/<str:pk>/', views.room, name="room"),
+    
 ] 
