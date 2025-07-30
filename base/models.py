7f2ad012492bc13d18918710932f4e46e93f7b16
TanujGupta001 <guptatanuj131124@gmail.com>
2025-07-30 17:02:59 +0530

    tanuj branch commit45

--- base/models.py
+++ base/models.py
@@ -1,33 +1,16 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db.models.deletion import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 
-# class Topic(models.Model):
-#     name = models.CharField(max_length=200)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-#
-# class Room(models.Model):
-#     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=200)
-#     description = models.TextField(null=True, blank=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
-#     tanuj = models.TextField(null=True)
-#     def __str__(self):
-#         return self.name
-#
-# class Message(models.Model):
-#     user = models.ForeignKey(User, on_delete=CASCADE)
-#     room = models.ForeignKey(Room, on_delete=CASCADE)
-#     body = models.TextField()
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.body[0:50]
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=50, unique=True)
+    college_email = models.EmailField(unique=True)
+    contact_number = models.CharField(max_length=15)
+    password =  models.CharField(max_length=15,blank=True)
+
+
+    def __str__(self):
+        return self.full_name
