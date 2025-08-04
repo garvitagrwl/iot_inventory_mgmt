57fbfa5f40f847922e81a0ee8a633f93697e5e15
Garvit Agrawal <garvitagrawal321.delhi@gmail.com>
2025-08-04 21:02:18 +0530

    DB Logic

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('student_reg/', views.student_reg, name="student_reg"),
     path('logout/', views.student_logout, name='logout'),
     path('student_dash/', include(('student_dash.urls', 'dash'), namespace='dash')),
-    path('login/studentdash/components', views.components, name="components"),
-   path('category/<str:category_key>/', views.category_items, name='category_items'),
+    
+   
 ] 
--- student_dash/urls.py
+++ student_dash/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'dash'
 urlpatterns = [
     path('request/', views.inventory_request, name='inventory_request'),
-    
+    path('login/studentdash/components', views.components, name="components"),
+    path('category/<str:category_key>/', views.category_items, name='category_items'),
+    path('submit-request/', views.submit_request, name='submit_request'),
 ]
